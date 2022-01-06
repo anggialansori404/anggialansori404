c839e266d5e2f29f74a3f1130f576f2b6d446787
Anggi Al Ansori <anggialansori.rpl4@gmail.com>
2022-01-06 08:27:06 +0700

    Update quotes.sh

--- quotes.sh
+++ quotes.sh
@@ -21,16 +21,16 @@ var() {
 }
 
 main() {
-    echo "## Hi, there! I'm Anggi Al'Ansori <img src="https://raw.githubusercontent.com/AnggaR96s/AnggaR96s/master/assets/Hi.gif" width="30px">
+    echo "## Hi, there! I'm Anggi Al'Ansori <img src=\"https://raw.githubusercontent.com/AnggaR96s/AnggaR96s/master/assets/Hi.gif"\ width=\"30px"\>
 
 <p align="center">
-<a href="https://github.com/anggialansori404"> <img src="https://img.shields.io/badge/-Github-000?style=flat&logo=Github&logoColor=white" /></a>
-<a href="https://www.instagram.com/anggialansori_"> <img src="https://img.shields.io/badge/-Instagram-c13584?style=flat&labelColor=c13584&logo=instagram&logoColor=white" /></a>
-<a href="mailto:anggialansori.rpl4@gmail.com"> <img src="https://img.shields.io/badge/-Gmail-c14438?style=flat&logo=Gmail&logoColor=white" /></p></a>
+<a href=\"https://github.com/anggialansori404"\> <img src=\"https://img.shields.io/badge/-Github-000?style=flat&logo=Github&logoColor=white"\ /></a>
+<a href=\"https://www.instagram.com/anggialansori_"\> <img src=\"https://img.shields.io/badge/-Instagram-c13584?style=flat&labelColor=c13584&logo=instagram&logoColor=white"\ /></a>
+<a href=\"mailto:anggialansori.rpl4@gmail.com"\> <img src=\"https://img.shields.io/badge/-Gmail-c14438?style=flat&logo=Gmail&logoColor=white"\ /></p></a>
 
 
 **Here is my portofolio as follows:**
-<img width="55%" align="right" alt="Github" src="https://raw.githubusercontent.com/AnggaR96s/AnggaR96s/master/assets/git-header.svg" />
+<img width=\"55%"\ align=\"right"\ alt=\"Github"\ src=\"https://raw.githubusercontent.com/AnggaR96s/AnggaR96s/master/assets/git-header.svg"\ />
 
 - A ROM maintainer for Redmi Note 10
 - Official maintainer for [Nusantara ROM](https://github.com/Nusantara-ROM)
@@ -39,13 +39,13 @@ main() {
 
 **Some programming languages that are already learned by me as follows:** 
 <p>
-  <a href="https://github.com/anggialansori404">
-    <img width="55%" align="right" alt="Anggi Al'Ansori's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=anggialansori404&show_icons=true&hide_border=true" />
+  <a href=\"https://github.com/anggialansori404"\>
+    <img width=\"55%"\ align=\"right"\ alt=\"Anggi Al'Ansori's GitHub Stats"\ src=\"https://github-readme-stats.vercel.app/api?username=anggialansori404&show_icons=true&hide_border=true"\ />
   </a>
 
-  <code><img width="10%" src="https://www.vectorlogo.zone/logos/mysql/mysql-ar21.svg"></code>
-  <code><img width="10%" src="https://www.vectorlogo.zone/logos/php/php-ar21.svg"></code>
-  <code><img width="10%" src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg"></code>
+  <code><img width=\"10%"\ src=\"https://www.vectorlogo.zone/logos/mysql/mysql-ar21.svg"\></code>
+  <code><img width=\"10%"\ src=\"https://www.vectorlogo.zone/logos/php/php-ar21.svg"\></code>
+  <code><img width=\"10%"\ src=\"https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg"\></code>
 
 </p>
 <hr>
